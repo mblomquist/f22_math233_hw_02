30a54a4a794434e8998f8d4d819081421e5b2cd1
mblomquist <Fr@$mb0627!>
2022-10-11 05:19:51 -0700

    Add RK2 version for find departure point.

--- src/advection/SL_method.cpp
+++ src/advection/SL_method.cpp
@@ -11,3 +11,27 @@ void SL_method::find_trajectory(int n, double &x_d, double &y_d, double dt) {
     x_d = x_0 - dt * vel_u[n];
     y_d = y_0 - dt * vel_v[n];
 }
+
+void SL_method::find_departure_2nd(int n, double & x_d, double & y_d, double dt) {
+    // RK2 Mid-Point Rule
+    double x_0 = sl_grid.x_from_n(n);
+    double y_0 = sl_grid.y_from_n(n);
+
+    double x_star = x_0 - 0.5 * dt * vel_u[n];
+    double y_star = y_0 - 0.5 * dt * vel_v[n];
+
+    // interpolate vel_u, vel_v at (x_star, y_star)
+    double vel_u_star = quadratic_interpolation(sl_grid, vel_u, x_star, y_star);
+    double vel_v_star = quadratic_interpolation(sl_grid, vel_v, x_star, y_star);
+
+    x_d = x_0 - dt * vel_u_star;
+    y_d = y_0 - dt * vel_v_star;
+}
+
+void SL_method::set_velocity(std::vector<double> &vel_u0, std::vector<double> &vel_v0) {
+    vel_u.resize(vel_v0.size());
+    vel_v.resize(vel_v0.size());
+
+    vel_u = vel_u0;
+    vel_v = vel_v0;
+}
--- src/advection/SL_method.h
+++ src/advection/SL_method.h
@@ -5,9 +5,9 @@
 #ifndef F22_MATH233_HW_02_SL_METHOD_H
 #define F22_MATH233_HW_02_SL_METHOD_H
 
-
-#include "../grid/Grid2d.h"
 #include <vector>
+#include "../grid/Grid2d.h"
+#include "../tools/math_tools.h"
 
 // Semi-Langrangian Method
 class SL_method {
@@ -17,6 +17,7 @@ private:
     std::vector<double> vel_u;
     std::vector<double> vel_v;
     void find_trajectory(int n, double & x_d, double & y_d, double dt);
+    void find_departure_2nd(int n, double & x_d, double & y_d, double dt);
 
 public:
     void set_grid(Grid2d & new_grid){sl_grid = new_grid;} // set grid
