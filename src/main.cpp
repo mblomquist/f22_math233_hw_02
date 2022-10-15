3a2f4211ce905cc06b2cc462d2271febb1ff1f40
mblomquist <Fr@$mb0627!>
2022-10-15 01:40:18 -0700

    Clean problem 1. Move SL_method into LS class, create functions for print and error measurement.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "grid/Grid2d.h"
-#include "advection/SL_method.h"
 #include "tools/math_tools.h"
 #include "levelset/LevelSet.h"
 #include "math.h"
@@ -16,30 +15,69 @@ void cf_vel0(double x, double y, double & vel_u, double & vel_v){
     vel_v = x;
 }
 
+void measure_error(std::vector<double> phi, std::vector<double> phi_0){
+
+    double l1, l2, linf;
+
+    l1   = norm_l1(phi, phi_0);
+    l2   = norm_l2(phi, phi_0);
+    linf = norm_linf(phi, phi_0);
+
+    std::cout << "L1 Norm: " << l1 << std::endl;
+    std::cout << "L2 Norm: " << l2 << std::endl;
+    std::cout << "Linf Norm: " << linf << std::endl;
+}
+
+void print_results_vtk(Grid2d newGrid, std::string fileName, double cfl, int itr,
+                       std::vector<double> phi, std::vector<double> vel_u, std::vector<double> vel_v){
+    // build fileName
+    fileName = fileName + std::to_string(cfl);
+    fileName.append("_");
+    fileName = fileName + std::to_string(itr);
+    fileName.append(".vtk");
+
+    // print to file
+    newGrid.print_VTK_format(fileName);
+    newGrid.print_VTK_format(vel_u, "vel_u", fileName);
+    newGrid.print_VTK_format(vel_v, "vel_v", fileName);
+    newGrid.print_VTK_format(phi, "phi", fileName);
+
+}
+
 int main() {
-    std::cout << "Hello, F22 MATH 233 Homework 2!" << std::endl;
+    std::cout << "Problem 1!\n" << std::endl;
 
     // Problem 1 - Semi-Lagrangian Method for the advection equation
 
+    // set grid size
     double xmin = -1.0;
     double xmax = 1.0;
     double ymin = -1.0;
     double ymax = 1.0;
 
+    // set grid points (keep square for now)
     int N = 100;
     int M = N;
 
-    Grid2d newGrid(N, M, xmin, xmax, ymin, ymax);
+    // set cfl (1/ratio), final time
+    double ratio = 0.1;
+    double t_final = 2.*PI;
 
-    double dx = newGrid.get_dx();
-
-    newGrid.print_VTK_format("../results/f22_hw02_problem1_0.vtk");
-
+    // create vectors for velocity, phi's
     std::vector<double> vel_u, vel_v, phi, phi_0;
     vel_u.resize(N*M);
     vel_v.resize(N*M);
     phi.resize(N*M);
+    phi_0.resize(N*M);
 
+    // initialize grid
+    Grid2d newGrid(N, M, xmin, xmax, ymin, ymax);
+
+    // compute dt for req ratio (1/CFL)
+    double dx = newGrid.get_dx();
+    double dt = dx/ratio;
+
+    // initialize vel_u, vel_v, phi_0
     for (int i = 0; i < N; i++){
         for (int j = 0; j < M; j++){
             cf_vel0(newGrid.x_from_n(newGrid.n_from_ij(i,j)),
@@ -47,108 +85,53 @@ int main() {
                     vel_u[newGrid.n_from_ij(i,j)],
                     vel_v[newGrid.n_from_ij(i,j)]);
 
-            phi[newGrid.n_from_ij(i,j)] = cf_phi(newGrid.x_from_n(newGrid.n_from_ij(i,j)),
+            phi_0[newGrid.n_from_ij(i,j)] = cf_phi(newGrid.x_from_n(newGrid.n_from_ij(i,j)),
                                                  newGrid.y_from_n(newGrid.n_from_ij(i,j)));
         }
     }
 
-    phi_0 = phi;
+    // print initial phi
+    print_results_vtk(newGrid, "../results/f22_hw02_problem3_", (1./ratio), 0,
+                      phi_0, vel_u, vel_v);
 
-    newGrid.print_VTK_format(vel_u, "vel_u", "../results/f22_hw02_problem1_0.vtk");
-    newGrid.print_VTK_format(vel_v, "vel_v", "../results/f22_hw02_problem1_0.vtk");
-    newGrid.print_VTK_format(phi, "phi", "../results/f22_hw02_problem1_0.vtk");
+    // create ls
+    LevelSet ls_phi(newGrid, phi_0);
 
-    double ratio = 0.1;
-    double dt = dx/ratio;
-
-    SL_method sl_solver;
-    sl_solver.set_grid(newGrid);
-    sl_solver.set_velocity(vel_u, vel_v);
-
-    double t_final = 2.*PI;
+    // compute steps
     int steps = floor(t_final / dt);
-    int pr, mprint;
-
-    mprint = 10;
-    pr = 0;
+    int iprint = 0;
+    int mprint = 1;
 
     for (int i = 0; i < steps + 2; i++){
 
         if (i*dt > t_final){
             double temp_t = (i-1)*dt;
             dt = t_final - (i-1)*dt;
-            std::cout << "Time: " << temp_t + dt << std::endl;
+            std::cout << "Percent Complete: " << (temp_t + dt) / t_final * 100. << " %," << std::endl;
         } else {
-            std::cout << "Time: " << i*dt << std::endl;
+            std::cout << "Percent Complete: " << i*dt/t_final * 100. << " %," << std::endl;
         }
 
-        sl_solver.update_sol(phi, dt);
+        ls_phi.advance_sl(vel_u, vel_v, dt);
 
         if (i % mprint == 0){
-            pr++;
-
-            std::string fileName = "../results/f22_hw02_problem1_";
-            std::string fileType = ".vtk";
-
-            fileName = fileName + std::to_string(pr);
-
-            fileName.append(fileType);
-
-            //std::cout << fileName << std::endl;
-
-            newGrid.print_VTK_format(fileName);
-            newGrid.print_VTK_format(vel_u, "vel_u", fileName);
-            newGrid.print_VTK_format(vel_v, "vel_v", fileName);
-            newGrid.print_VTK_format(phi, "phi", fileName);
+            iprint++;
+            ls_phi.getPhi(phi);
+            print_results_vtk(newGrid, "../results/f22_hw02_problem3_", (1./ratio), iprint,
+                              phi, vel_u, vel_v);
         }
     }
 
+    // update phi
+    ls_phi.getPhi(phi);
+
+    // measure the error
+    std::cout << "\nProblem 1 - Measured Error for CFL " << (1./ratio) << std::endl;
+    measure_error(phi, phi_0);
 
     // Problem 2 - Reinitialization Equation
-    double l1, l2, linf;
-
-    l1   = norm_l1(phi, phi_0);
-    l2   = norm_l2(phi, phi_0);
-    linf = norm_linf(phi, phi_0);
-
-    std::cout << "\nFor ratio: " << ratio << "\n" << std::endl;
-    std::cout << "L1 Norm: " << l1 << std::endl;
-    std::cout << "L2 Norm: " << l2 << std::endl;
-    std::cout << "Linf Norm: " << linf << std::endl;
 
     // Problem 3 - Level Set Method
-    LevelSet ls_phi(newGrid, phi_0);
-    std::vector<double> p2_phi;
-    p2_phi.resize(phi_0.size());
-    ls_phi.getPhi(p2_phi);
-
-    std::string fileName = "../results/f22_hw02_problem2_";
-    std::string fileType = ".vtk";
-
-    fileName = fileName + std::to_string(0);
-    fileName.append(fileType);
-
-    newGrid.print_VTK_format(fileName);
-    newGrid.print_VTK_format(vel_u, "vel_u", fileName);
-    newGrid.print_VTK_format(vel_v, "vel_v", fileName);
-    newGrid.print_VTK_format(p2_phi, "phi", fileName);
-
-    for (int i = 0; i < 100; i++){
-        ls_phi.advance_sl(vel_u, vel_v, .1);
-    }
-
-    fileName = "../results/f22_hw02_problem2_";
-
-    fileName = fileName + std::to_string(1);
-    fileName.append(fileType);
-
-    ls_phi.getPhi(p2_phi);
-
-    newGrid.print_VTK_format(fileName);
-    newGrid.print_VTK_format(vel_u, "vel_u", fileName);
-    newGrid.print_VTK_format(vel_v, "vel_v", fileName);
-    newGrid.print_VTK_format(p2_phi, "phi", fileName);
-
 
     // Problem 4 - Extra Credit
 
